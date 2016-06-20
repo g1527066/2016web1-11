7b571a8f6fb798cecfab450fe32ec2d0a40cc58c
nyanko-sankaku <g1527066@st.t-kougei.ac.jp>
2016-06-20 17:06:06 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -1,5 +1,5 @@
 console.log('こんにちは世界！');
 
 //答えを用意
-var number =Math.floor(Math.random()*3);
-var answer =parseInt(window.prompt('数あてゲーム'))
+//var number =Math.floor(Math.random()*3);
+//var answer =parseInt(window.prompt('数あてゲーム'))
