ff7adbf9e045d5b11c30ed1eb95448a67e7d0430
nyanko-sankaku <g1527066@st.t-kougei.ac.jp>
2016-06-20 18:01:45 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -2,17 +2,20 @@
 while(1){
 var number =Math.floor(Math.random()*3);
 var answer =parseInt(window.prompt('数あてゲーム。0~2の数字を入力してください！'));
-var flag=false;
+var flag=0;
 
 var message;
 if(answer===number){
   message='あたり！';
-flag=true;
+flag=1;
 }else if(answer<number){
   message='残念！もっと大きいです';
+flag=2;
+  
+  
 }else if(answer>number){
     message='残念！もっと小さいです';
-    document.write('<img src="./img/51bfac164e73a8ede3755945ce8b9e7f.jpg" width="400" height="260" />');
+flag=3;
 }else{
   message='0~3の数字を入力してください';
 }
@@ -20,9 +23,14 @@ flag=true;
 
 //結果を表示
 document.getElementById('choice').textContent=message;
-if(flag){
+if(flag==1){
+  document.write('<img src="./img/51bfac164e73a8ede3755945ce8b9e7f.jpg" width="400" height="260" />');
 break;
 }
+else if(flag==2)
+{
+    document.write('<img src="./img/51bfac164e73a8ede3755945ce8b9e7f.jpg" width="400" height="260" />');
 }
 
-document.write('<img src="./img/51bfac164e73a8ede3755945ce8b9e7f.jpg" width="400" height="260" />');
+}
+
