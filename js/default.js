147361c209fa8fc0ec17f515042645f004e47d82
nyanko-sankaku <g1527066@st.t-kougei.ac.jp>
2016-06-20 17:23:40 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -1,10 +1,12 @@
 //答えを用意
+while(1){
 var number =Math.floor(Math.random()*3);
 var answer =parseInt(window.prompt('数あてゲーム。0~2の数字を入力してください！'));
 
 var message;
 if(answer===number){
   message='あたり！';
+  break;
 }else if(answer<number){
   message='残念！もっと大きいです';
 }else if(answer>number){
@@ -13,6 +15,6 @@ if(answer===number){
   message='0~3の数字を入力してください';
 }
 
-
+}
 //結果を表示
 document.getElementById('choice').textContent=message;
